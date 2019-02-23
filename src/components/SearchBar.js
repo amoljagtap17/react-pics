b2bcecca2e6b4dbd36a2f26bbf667d1a33138c0b
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-02-24 01:03:21 +0530

    Add controlled state to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  state = {
+    term: ''
+  }
+
+  onInputChange = ({ target: { value } }) => {
+    this.setState({ term: value })
+  }
+
+  onFormSubmit = event => {
+    event.preventDefault()
+
+    this.props.onSubmit(this.state.term)
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label htmlFor="">Image Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
